9fa7725c9692bc309eb902bb44db2c7652322954
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-03-18 15:22:33 +0530

    CellAnnotation.java

--- src/test/java/com/test/section_component/CellAnnotation.java
+++ src/test/java/com/test/section_component/CellAnnotation.java
@@ -50,7 +50,7 @@ public class CellAnnotation {
 		
 			DesiredCapabilities dc = DesiredCapabilities.chrome();
 		
-			URL url = new URL("http://172.20.23.7:5555/wd/hub");
+			URL url = new URL("http://172.12.20.99:4444/wd/hub");
 		
 			driver = new RemoteWebDriver(url, dc);
 		
@@ -123,7 +123,7 @@ public class CellAnnotation {
 		
 			if (emailInput != null && emailInput.isDisplayed()) {
 		
-			emailInput.sendKeys("teamsoftware457@gmail.com");
+			emailInput.sendKeys("softwareteam45@gmail.com");
 		
 			System.out.println("Email was entered successfully.");
 		
